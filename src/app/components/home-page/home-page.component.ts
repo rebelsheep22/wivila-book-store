2263b1b45ba1be093b0f2ef4a3d350935c894232
Nika Tsivilashvili <wivila2000nika@gmail.com>
2022-02-14 23:55:49 +0400

    add backend

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { faDollar } from '@fortawesome/free-solid-svg-icons';
+import { BookService } from 'src/services/book.service';
 
 @Component({
   selector: 'app-home-page',
@@ -6,10 +8,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
+  faDollar= faDollar;
+books$: any;
+splicedBooks$:any;
+novels$:any;
+comics$:any;
+programming$:any;
 
-  constructor() { }
+  constructor(
+    private bookService: BookService
+  ) { }
 
   ngOnInit(): void {
+    this.getBooks();
+    this.getComics();
+    this.getProgramming();
+    this.getNovels();
+  }
+  getBooks(){
+    return this.bookService.getBooks().subscribe((res)=>{
+     this.books$ = res;
+    })
+  }
+  getComics(){
+    return this.bookService.getComics().subscribe((res: any)=>{
+      this.comics$ = res.splice(0,4);
+    })
+  }
+  getProgramming(){
+    return this.bookService.getProgramming().subscribe((res:any)=>{
+      this.programming$ = res.splice(0,4);
+    })
+  }
+  getNovels(){
+    return this.bookService.getNovels().subscribe((res:any)=>{
+      this.novels$ = res.splice(0,4);
+    })
   }
 
 }
